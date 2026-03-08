8123b06f4d265c59c41ff6d8b2dc356422090a03
RaghaNandini <bainaraghanandini@gmail.com>
2026-03-08 13:44:08 +0530

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,5 +1,4 @@
 import logging
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -11,26 +10,21 @@ login = LoginManager()
 def create_app():
     app = Flask(__name__)
 
-    # Secret key
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', config.SECRET_KEY)
+    app.config['SECRET_KEY'] = config.SECRET_KEY
 
-    # Azure SQL configuration
     app.config['SQLALCHEMY_DATABASE_URI'] = (
         f"mssql+pyodbc://{config.SQL_USER_NAME}:{config.SQL_PASSWORD}"
         f"@{config.SQL_SERVER}:1433/{config.SQL_DATABASE}"
-        "?driver=ODBC+Driver+18+for+SQL+Server"
+        "?driver=ODBC+Driver+17+for+SQL+Server"
     )
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # Logging
     logging.basicConfig(level=logging.INFO)
 
-    # Initialize extensions
     db.init_app(app)
     login.init_app(app)
 
-    # Register routes
     from FlaskWebProject.views import bp
     app.register_blueprint(bp)
 
