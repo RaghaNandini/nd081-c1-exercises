cc08e34360eba48f73102a70fbef6fd717e8f8f3
RaghaNandini <bainaraghanandini@gmail.com>
2026-03-08 11:04:50 +0530

    Simplify application by removing DB logic
    Removed database connection logic and user route, added a simple home route.

--- application.py
+++ application.py
@@ -1,34 +1,7 @@
-    
-from flask import Flask, jsonify
-from sqlalchemy import text
-from FlaskWebProject.db import engine
-import sys
-sys.path.append("..")
-import config
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    try:
-        with engine.connect() as conn:
-            result = conn.execute(text("SELECT 1"))  
-            return f"✅ DB connected successfully, query result: {result.scalar()}"
-    except Exception as e:
-        return f"❌ DB connection failed: {e}"
-
-@app.route('/users')
-def users():
-    try:
-        with engine.connect() as conn:
-            result = conn.execute(text("SELECT * FROM users")) 
-            rows = result.fetchall()
-            users_list = [dict(row._mapping) for row in rows]
-            return jsonify(users_list)
-    except Exception as e:
-        return jsonify({"error": str(e)})
-'''
-if __name__ == "__main__":
-    app.run(debug=True)'''
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    return "Hello World"
