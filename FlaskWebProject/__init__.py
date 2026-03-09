21a8b8035b86988732318bb81ee02ee8e42f8e16
Ragha Nandini <bainaraghanandini@gmail.com>
2026-03-09 16:31:18 +0530

    Update project files and fix deployment

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,34 +1,27 @@
-import logging
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-import config
+import os
+from . import config
 
 db = SQLAlchemy()
-login = LoginManager()
 
 def create_app():
 
     app = Flask(__name__)
 
-    # Secret key
-    app.config['SECRET_KEY'] = config.SECRET_KEY
+    app.config["SECRET_KEY"] = config.SECRET_KEY
 
-    # Azure SQL connection
-    app.config['SQLALCHEMY_DATABASE_URI'] = (
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
         f"mssql+pyodbc://{config.SQL_USER_NAME}:{config.SQL_PASSWORD}"
         f"@{config.SQL_SERVER}:1433/{config.SQL_DATABASE}"
         "?driver=ODBC+Driver+17+for+SQL+Server"
     )
 
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    logging.basicConfig(level=logging.INFO)
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
-    login.init_app(app)
 
-    from FlaskWebProject.views import bp
+    from .views import bp
     app.register_blueprint(bp)
 
     return app
