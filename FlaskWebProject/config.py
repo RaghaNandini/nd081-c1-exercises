b583202d0cfc6e9f389a787e97bcf3eb481f19ab
RaghaNandini <bainaraghanandini@gmail.com>
2026-03-10 12:15:23 +0530

    Update config.py

--- FlaskWebProject/config.py
+++ FlaskWebProject/config.py
@@ -1,39 +1,18 @@
 import os
 
-# -------------------------
-# Flask Secret Key
-# -------------------------
-SECRET_KEY = os.environ.get("SECRET_KEY", "dev_secret_key")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# -------------------------
-# Azure SQL Database
-# -------------------------
+# SQL DATABASE
 SQL_SERVER = os.environ.get("SQL_SERVER")
 SQL_DATABASE = os.environ.get("SQL_DATABASE")
 SQL_USER_NAME = os.environ.get("SQL_USER_NAME")
 SQL_PASSWORD = os.environ.get("SQL_PASSWORD")
 
-SQLALCHEMY_DATABASE_URI = (
-    f"mssql+pyodbc://{SQL_USER_NAME}:{SQL_PASSWORD}"
-    f"@{SQL_SERVER}:1433/{SQL_DATABASE}"
-    "?driver=ODBC+Driver+17+for+SQL+Server"
-)
+# BLOB STORAGE
+BLOB_CONNECTION_STRING = os.environ.get("BLOB_CONNECTION_STRING")
+BLOB_CONTAINER = os.environ.get("BLOB_CONTAINER")
 
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# -------------------------
-# Azure AD Authentication
-# -------------------------
+# MICROSOFT LOGIN
 CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 TENANT_ID = os.environ.get("TENANT_ID")
-
-AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
-REDIRECT_PATH = "/getAToken"
-SCOPE = ["User.Read"]
-
-# -------------------------
-# Azure Blob Storage
-# -------------------------
-BLOB_CONNECTION_STRING = os.environ.get("BLOB_CONNECTION_STRING")
-BLOB_CONTAINER = os.environ.get("BLOB_CONTAINER")
