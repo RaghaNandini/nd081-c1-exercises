8a5c80aebb191d0d050c2b6cbcd22e40f47afb8e
RaghaNandini <bainaraghanandini@gmail.com>
2026-03-10 20:46:40 +0530

    Update db.py

--- FlaskWebProject/db.py
+++ FlaskWebProject/db.py
@@ -22,8 +22,6 @@ params = urllib.parse.quote_plus(
 )
 
 # Create SQLAlchemy engine
-<<<<<<< HEAD
+
 engine = create_engine(f"mssql+pyodbc:///?odbc_connect={params}")
-=======
-engine = create_engine(f"mssql+pyodbc:///?odbc_connect={params}")
->>>>>>> f5f8fba (Fix Azure SQL connection)
+
